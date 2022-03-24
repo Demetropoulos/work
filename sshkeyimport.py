5a9a6248ca7174ecdeadd82b0df1bf7bdd14acf4
PheonixNyte <8531917+PheonixNyte@users.noreply.github.com>
2022-03-24 14:51:45 -0400

    v1.2 functions and cleaned up

--- sshkeyimport.py
+++ sshkeyimport.py
@@ -1,59 +1,31 @@
 import paramiko
 import time
-from getpass import getpass
-
-
-localkey = "/home/user/.ssh/id_rsa.pub"
-remotekey = "id_rsa.pub"
 
 conft = 'conf t'
 shrun = 'sh run'
 space = ' '
-breakcommand = 'q'
+quit = 'q'
 
-command = 'username awxuser password 0 awxpassword role network-admin'
-command2 = 'username awxuser sshkey xxxHASHxxx'
+nocommand1 = 'no username awxuser password 0 awxpass role network-admin'
+nocommand2 = 'no username awxuser sshkey'
 
-# Test commands
-#command = "sh ver"
-#command2 = "do sh ver"
+command0 = 'sh int desc | i 1/1'
+command1 = 'username awxuser2 password 0 awxpass role network-admin'
+command2 = 'username awxuser2 sshkey ssh-rsa xxxHASHxxx
 
 # Pulls username and pw from file because I'm lazy
-with open('config.txt') as f:
-    lines = f.readlines()
-    username = lines[0].strip()
-    password = lines[1].strip()
+#with open('config.txt') as f:
+#    lines = f.readlines()
+#    username = lines[0].strip()
+#    password = lines[1].strip()
 #    print(f"user: {username}, pass: {password}")
 
-# Opens file with IP list of devices to configure
-with open('dclist.txt') as file:
-    for line in file:
-        ip = line.strip()
-#        print("scp " + localkey + " awxuser@" + ip + ":/" + remotekey)
 
-# Create object of SSHClient and
-# connecting to SSH
-        ssh = paramiko.SSHClient()
-        print(ip)
-
-# Adding new host key to the local
-# HostKeys object(in case of missing)
-# AutoAddPolicy for missing host key to be set before connection setup.
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-# Connect via SSH
-        ssh.connect(ip, port=22, username=username, password=password, timeout=3)
-
-# Open Interactive Shell
-        connection = ssh.invoke_shell()
-
-# Send commands, takes an ssh.invoke_shell object and
-# a list of commands to be run. \r\n is sent after each command
-# Returns full buffer of returned data
-        def send_command(connection, commands):
+def send_command(connection, commands):
             for command in commands:
                 connection.send(command)
                 connection.send('\n')
+                print(f"Sent command: ...{command}...")
                 time.sleep(2)
             while not connection.recv_ready():
                 time.sleep(1)
@@ -61,25 +33,43 @@ with open('dclist.txt') as file:
             while connection.recv_ready():
                 buff = connection.recv(65536).decode('utf-8')
                 lbuff += buff
+                print("Receiving output...")
             return lbuff
 
-        data = send_command(connection, [conft,command,command2,shrun,space,breakcommand])
-        print(data)
+### Function to SSH ###
+# Connects to device via SSH.
+# Also enters a single command and returns output.
+def sshinteractive(ip,commands):
+    with paramiko.SSHClient() as ssh:
+        print(f"Connecting to {ip} via ssh...")
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(ip, port=22, username='awxuser2',
+                    key_filename='/home/user/.ssh/id_rsa.pub', timeout=3)
+        connection = ssh.invoke_shell()
+        data = send_command(connection,commands)
+        return data
 
-# Enter config terminal mode on SSH terminal using exec_command
-#        stdin, stdout, stderr = ssh.exec_command(conft)
-#        print(stdout.readlines())
+# Opens file with IP list of devices to configure
+with open('dclist.txt') as file:
+    for line in file:
+        ip = line.strip()
+        #print("scp " + localkey + " awxuser@" + ip + ":/" + remotekey)
+        print(sshinteractive(ip,[conft,nocommand1,nocommand2,shrun,space,quit]))
 
-# Execute command and print output
-#        stdin, stdout, stderr = ssh.exec_command(command)
-#        print(stdout.readlines())
+        # Enter config terminal mode on SSH terminal using exec_command
+        #stdin, stdout, stderr = ssh.exec_command(command0)
+        #print(stdout.readlines())
 
-# Execute another command and print output
-#        stdin, stdout, stderr = ssh.exec_command(command2)
-#        print(stdout.readlines())
+        # Execute command and print output
+        #stdin, stdout, stderr = ssh.exec_command(command1)
+        #print(stdout.readlines())  
 
-# Close ssh session
-        ssh.close()
+        # Execute another command and print output
+        #stdin, stdout, stderr = ssh.exec_command(command2)
+        #print(stdout.readlines())
 
-# Break from loop while testing
-#        break
+        # Break from loop while testing        
+        #break
+
+# End of Script
+print("END OF SCRIPT")
