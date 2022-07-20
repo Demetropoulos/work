14b09e23fbd02723a4a12648d7dc3ecfef8d3227
PheonixNyte <8531917+PheonixNyte@users.noreply.github.com>
2022-07-20 16:12:11 -0400

    Update searchmac.py

--- searchmac.py
+++ searchmac.py
@@ -1,57 +1,6 @@
 import paramiko
 import yaml
 
-print("\nThis will help you locate a switch and port from a MAC Address.\n",
-        "Please ensure you're searching for a wired device.\n",
-        "If wireless, please search Cisco Prime.\n")
-
-siteknown = input("Do you know the site where this MAC Address is located? y/n: ")
-if siteknown == 'y':
-    siteknown = True
-else:
-    siteknown = False
-
-if siteknown == True:
-    print("Here is our site list: \n")
-    
-    with open('sites.yml','r') as s:
-        listsites = yaml.safe_load(s)
-        sites_dict = listsites['sites']
-        for site in sites_dict:
-            singlesite = sites_dict[site]
-            print(f"{singlesite} - {singlesite['siteAddress']}")
-            #print(item + '\n')
-        picksite = input("Please choose the number corresponding to your site: ")
-    mac = input("Please enter the MAC Address (abcd.ef12.3456, or ab-cd-ef-12-34-56, or ab:cd:ef:12:34:56): ")
-    shmac = 'sh mac add add ' + mac
-    
-    if picksite <= 9:
-        macoutput = sshconnectdc(picksite,shmac)
-    else:
-        macoutput = sshconnectcampus(picksite,shmac)
-    
-    print(macoutput)
-
-else:
-    print("\nSearching for a MAC Address throughout the IDN?\n",
-          "Please provide the MAC Address and sit tight.\n",
-          "This might take a few minutes.\n")
-    mac = input("Please enter the MAC Address (abcd.ef12.3456, or ab-cd-ef-12-34-56, or ab:cd:ef:12:34:56): ")
-    shmac = 'sh mac add add ' + mac
-    with open('sites.yml','r') as s:
-        sites = yaml.safe_load(s)
-        for site in sites:
-            print(f"Trying {site}...")
-            if picksite <= 9:
-                macoutput = sshconnectdc(site['siteRouterA'],shmac)
-            else:
-                macoutput = sshconnectcampus(site['siteRouterA'],shmac)
-            if mac in macoutput:
-                print(f"MAC is at {site}")
-            else:
-                print(f"MAC not at {site}")
-
-
 def sshconnectcampus(ip,command):
     with paramiko.SSHClient() as ssh:
         # print("Open SSH.")
@@ -71,3 +20,56 @@ def sshconnectdc(ip,command):
         stdin, stdout, stderr = ssh.exec_command(command)
         # print("Closing SSH.")
         return stdout.readlines()
+
+print("\nThis will help you locate a switch and port from a MAC Address.\n",
+        "Please ensure you're searching for a wired device.\n",
+        "If wireless, please search Cisco DNAC.\n")
+
+siteknown = input("Do you know the site where this MAC Address is located? y/n: ")
+if siteknown == 'y':
+    siteknown = True
+else:
+    siteknown = False
+
+if siteknown == True:
+    print("\nHere is our site list: \n")
+    
+    with open('sites.yml','r') as s:
+        listsites = yaml.safe_load(s)
+        sites_dict = listsites['sites']
+        for site in sites_dict:
+            singlesite = sites_dict[site]
+            print(f"{singlesite['siteID']} - {singlesite['siteName']} - {singlesite['siteAddress']}")
+            #print(item + '\n')
+        picksite = input("\nPlease choose the number corresponding to your site: ")
+    mac = input("\nPlease enter the MAC Address (abcd.ef12.3456, or ab-cd-ef-12-34-56, or ab:cd:ef:12:34:56): ")
+    shmac = 'sh mac add add ' + mac
+
+    selection = int(picksite)
+    if selection <= 9:
+        ip = singlesite['siteRouterA']
+        print(f"\nThis is the router IP: {ip}\n")
+        macoutput = sshconnectdc(ip,shmac)
+    else:
+        macoutput = sshconnectidn(ip,shmac)
+    
+    print(macoutput)
+
+else:
+    print("\nSearching for a MAC Address throughout the IDN?\n",
+          "Please provide the MAC Address and sit tight.\n",
+          "This might take a few minutes.\n\n")
+    mac = input("Please enter the MAC Address (abcd.ef12.3456, or ab-cd-ef-12-34-56, or ab:cd:ef:12:34:56): ")
+    shmac = 'sh mac add add ' + mac
+    with open('sites.yml','r') as s:
+        sites = yaml.safe_load(s)
+        for site in sites:
+            print(f"Trying {site}...")
+            if picksite <= 9:
+                macoutput = sshconnectdc(site['siteRouterA'],shmac)
+            else:
+                macoutput = sshconnectidn(site['siteRouterA'],shmac)
+            if mac in macoutput:
+                print(f"MAC is at {site}\n")
+            else:
+                print(f"MAC not at {site}\n")
