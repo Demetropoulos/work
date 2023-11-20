322855f097dc2f30fa6d62fb27a4836aec4dbb76
Dean <8531917+Demetropoulos@users.noreply.github.com>
2023-11-20 15:02:01 -0500

    Update badip.py

--- badip.py
+++ badip.py
@@ -8,7 +8,7 @@ def remove_bad_ip_entries(unique_ips):
 
 def format_unique_ips(unique_ips):
     "Format unique IPs by removing special characters."
-    return [ip.replace('*', '').replace('#', '') for ip in unique_ips if ip.strip()]
+    return [ip.replace('*', '').replace('#', '').replace(' ', '') for ip in unique_ips if ip.strip()]
 
 def format_sw_result(unique_ips):
     "Format SW result."
@@ -18,9 +18,9 @@ def format_mm_result(unique_ips):
     "Format MineMeld result."
     return ["#DTS-9537\n{}".format(ip) for ip in unique_ips]
 
-def remove_ips_from_panexport(panexport_ips, ip_ips):
-    "Remove IPs from panexport_ips that are present in ip_ips."
-    return [ip for ip in panexport_ips if ip not in ip_ips]
+def remove_ips_from_panexport(panexport_ips, previous_ips):
+    "Remove IPs from panexport_ips that are present in previous_ips."
+    return [ip for ip in panexport_ips if ip not in previous_ips]
 
 def write_to_file(file_path, data):
     "Write data to a file and print contents to the screen."
@@ -31,10 +31,10 @@ def write_to_file(file_path, data):
     except IOError:
         print("Error writing to file: {}".format(file_path))
 
-def write_no_print(file_path, data):
-    "Write data to a file and DO NOT print contents to the screen."
+def append_to_file(file_path, data):
+    "Append data to previous.lst file."
     try:
-        with open(file_path, 'w') as output_file:
+        with open(file_path, 'a') as output_file:
             output_file.write('\n'.join(data))
     except IOError:
         print("Error writing to file: {}".format(file_path))
@@ -47,26 +47,30 @@ mm_output_file_path = "mm.lst"
 jira_output_file_path = "jira.lst"
 
 try:
-    with open(previous_list_path, 'r') as previous_file:
-        previous_list = [line.strip() for line in previous_file.readlines()]
-except IOError:
-    print("Error reading file: {}".format(previous_list_path))
-    previous_list = []
-
-try:
     with open(input_file_path, 'r') as panexport_file:
         ip_addresses_list = [line.strip() for line in panexport_file.readlines()]
 except IOError:
     print("Error reading file: {}".format(input_file_path))
     ip_addresses_list = []
 
+try:
+    with open(previous_list_path, 'r') as previous_file:
+        previous_list = [line.strip() for line in previous_file.readlines()]
+except IOError:
+    print("Error reading file: {}".format(previous_list_path))
+    previous_list = []
+
 # Process duplicate IPs and remove 'BadIP (never expire)' entries
 unique_ips = remove_duplicates(ip_addresses_list)
 filtered_ips = remove_bad_ip_entries(unique_ips)
 formatted_ips = format_unique_ips(filtered_ips)
+
+# Process duplicate IPs from previous list that have already been migrated
 filtered_panexport_list = remove_ips_from_panexport(formatted_ips, previous_list)
 write_to_file(jira_output_file_path, filtered_panexport_list)
-write_no_print(previous_list_path, filtered_panexport_list)
+
+# Write new IP list to previous file to be used on next run
+append_to_file(previous_list_path, filtered_panexport_list)
 
 # Format SW result and write to file
 sw_result = format_sw_result(filtered_panexport_list)
